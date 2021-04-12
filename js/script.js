586def434e5df0f873d2e3167ba4bc39c103edea
araujovaleria <araujovaleria.arte@gmail.com>
2021-04-12 01:16:38 -0300

    criar meu curriculo

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//var foto = document.querySelector(".foto")
+
+//foto.addEventListener("mouseenter", entrar)
+//foto.addEventListener("mouseout", sair)
+
+//function entrar (){
+    //foto.style.background = "rgba(255, 255, 255, 0.932)"
+
+//}
